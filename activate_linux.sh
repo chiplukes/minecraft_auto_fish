e4c79ec742ed24bce33537103c9423e37141e859
Chip Lukes <34627158+chiplukes@users.noreply.github.com>
2023-09-14 18:21:33 -0600

    added scripts to setup and activate virtual environment

---
+++ activate_linux.sh
@@ -0,0 +1,1 @@
+sh ./.venv/bin/activate
---
+++ setup_linux.sh
@@ -0,0 +1,7 @@
+python3 -m venv .venv
+sh ./.venv/bin/activate
+
+python3 -m pip install -e pyautogui
+python3 -m pip install -e pillow
+python3 -m pip install -e opencv-python
+python3 -m pip install -e mss
